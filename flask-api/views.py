88fb96b0a9b1a453dc192a9311d515a2e8d2d529
VENKATESH <vchary78@gmail.com>
2019-03-07 14:41:42 +0530

    flask api to perform back ground data tasks

---
+++ flask-api/views.py
@@ -0,0 +1,20 @@
+from app import app, db
+from flask import jsonify, request, Response
+from models import *
+import re
+from flask_cors import CORS, cross_origin
+
+def isValidEmail(email):
+    if len(email) > 7:
+        if re.match("^.+@([?)[a-zA-Z0-9-.]+.([a-zA-Z]{2,3}|[0-9]{1,3})(]?)$", email) != None:
+            return True
+        return False
+
+@app.route("/home")
+def intro():
+    return '<p>intro</p>'
+
+
+@app.route('/')
+def home():
+    return '<h2> WEL-COME</h2>'
